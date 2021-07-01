136551d199ca8e7a4f431d3c7c8ca3e0221ea55f
Aaron Garner <aaron.garner.1982@gmail.com>
2021-07-01 11:33:37 -0700

    Updated .gitignore to allow build and pushing build.

---
+++ build/precache-manifest.f89b2bbc932e207ffe9c8207e0d752ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a12e0d2396a8a620ed48c2ffc25ee5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e1b10953664a0178a68d",
+    "url": "./static/css/2.e807eb6e.chunk.css"
+  },
+  {
+    "revision": "a9076fe64c16b6e4fde6",
+    "url": "./static/css/main.c1c530ce.chunk.css"
+  },
+  {
+    "revision": "e1b10953664a0178a68d",
+    "url": "./static/js/2.f4036a0c.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "./static/js/2.f4036a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9076fe64c16b6e4fde6",
+    "url": "./static/js/main.3267d42c.chunk.js"
+  },
+  {
+    "revision": "4c59c965b773fd5bc88d",
+    "url": "./static/js/runtime-main.5e045ca0.js"
+  },
+  {
+    "revision": "044f66d1c5b82e01acc1053e7638a9f1",
+    "url": "./static/media/River Rocks.044f66d1.jpg"
+  }
+]);
