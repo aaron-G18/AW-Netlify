2a4fbf52c19950d0f03011141ef5a3b168833941
Aaron Garner <aaron.garner.1982@gmail.com>
2021-07-03 16:11:45 -0700

    Added burger menu to About page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,25 @@
 import React, { Component} from 'react';
 import Header from "../components/header";
 import Content from "../components/about";
-
+import Burger from "../components/burger";
+import Menu from "../components/burger-menu";
 
 class About extends Component {
   constructor(props) {
     super(props);
     this.state = {
       homePage: false,
-      activePage: "about"
+      activePage: "about",
+      menuOpen: false
+    };
+  };
+
+  // Function to toggle 'menuOpen' in state for use as onClick function in burger (hamburger menu)
+  setOpen = () => {
+    if (this.state.menuOpen === false) {
+      this.setState({menuOpen: true})
+    } else {
+    this.setState({menuOpen: false})
     };
   };
 
@@ -19,6 +30,14 @@ class About extends Component {
         <Header 
           homePage={this.state.homePage}
           activePage={this.state.activePage}
+        />
+        <Burger 
+          menuOpen={this.state.menuOpen}
+          onClick={this.setOpen}
+          homePage={this.state.homePage}
+        />
+        <Menu 
+          menuOpen={this.state.menuOpen}
         />
         <Content />
       </div>
