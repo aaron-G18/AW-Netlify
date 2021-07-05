9b97a8c06aec3b44099c174487b2edc0283bee6b
Aaron Garner <aaron.garner.1982@gmail.com>
2021-07-05 13:47:32 -0700

    Updated about page for mobile devices. Reversed order of HTML to put image on top of paragraph and applied styling for mobile responsiveness.

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -3,12 +3,9 @@ import "./style.css";
 
 function Content(props) {
     return(
-    <div>
-        
-        <p id="aw-about">I’m Aaron Williams, photographer and filmmaker based in Berlin, Germany.</p>
-        
-        
+    <div id="about-content">
         <img id="aaron" src="./images/Aaron_Williams.jpg" alt="Aaron Williams portrait"></img>
+        <p id="aw-about">I’m Aaron Williams, photographer and filmmaker based in Berlin, Germany.</p>
     </div>
     );
 };
