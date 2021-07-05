c5d78f8c9143c1f4b37a8592aa54a8a9c0d2e5d6
Aaron Garner <aaron.garner.1982@gmail.com>
2021-07-05 14:06:08 -0700

    Updated home page for mobile devices. Resized and stacked the center navigation buttons.

--- src/components/work-nav/index.js
+++ src/components/work-nav/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 function WorkNav(props) {
     return (
         <div id="work-navigation">
-            <a className="work-nav" href={process.env.PUBLIC_URL + "/portrait"}>Portrait</a>
-            <a className="work-nav" href={process.env.PUBLIC_URL + "/life"}>Life</a>
+            <a id="portrait-nav-button" className="work-nav" href={process.env.PUBLIC_URL + "/portrait"}>Portrait</a>
+            <a id="life-nav-button" className="work-nav" href={process.env.PUBLIC_URL + "/life"}>Life</a>
         </div>
     );
 };
