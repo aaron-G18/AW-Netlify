df95457066e8da0459a99c572f384cfe73e5d775
Aaron Garner <aaron.garner.1982@gmail.com>
2021-08-17 12:11:24 -0700

    updated IG url link.

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -10,7 +10,7 @@ function Navbar(props) {
                 <li><a className={props.homePage ? "white" : "dark"} id={props.activePage === "work" ? "active" : "inactive"} href="/">Work</a></li>
                 <li><a className={props.homePage ? "white" : "dark"} id={props.activePage === "about" ? "active" : "inactive"} onClick={props.onClick} href="/about">About</a></li>
                 <li><a className={props.homePage ? "white" : "dark"} id={props.activePage === "contact" ? "active" : "inactive"} href="/contact">Contact</a></li>
-                <li><a className={props.homePage ? "white" : "dark"} href="https://www.instagram.com/1inawilliams_photography/" id="ig-icon"><FiInstagram /></a></li>
+                <li><a className={props.homePage ? "white" : "dark"} href="https://www.instagram.com/aaronwilliams_media/" id="ig-icon"><FiInstagram /></a></li>
             </ul>
         </div>
     );
